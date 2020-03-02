e9c8377a99ab5900dfb91275964b4eb7db1c336a
ZJ860619 <zj_0627@163.com>
2020-03-02 23:24:20 +0800

    项目简单搭建 test

---
+++ src/test/java/cn/bdqn/demo/DemoApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.bdqn.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
